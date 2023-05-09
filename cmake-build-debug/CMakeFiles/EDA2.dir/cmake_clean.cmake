20e76f41b70dfdb06645e87a7d5d8759d764ac7f
gori <gorigrs@gmail.com>
2023-05-09 23:04:44 +0200

    Corregir llista-Gori

---
+++ cmake-build-debug/CMakeFiles/EDA2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDA2.dir/main.c.o"
+  "CMakeFiles/EDA2.dir/main.c.o.d"
+  "EDA2"
+  "EDA2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EDA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
