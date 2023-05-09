ae86fa8adf93641b9049a38fd0bdb7ff51f8f73c
Marcel-Medina <marcel.medina01@estudiant.upf.edu>
2023-05-09 16:44:33 +0200

    Implemented a temporary menu, point 2, 3 and 4
    Marcel

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#define MAX_LIKINGS = 5
+
+typedef struct{
+    char Username;
+    int Age;
+    char Mail;
+    char Location;
+    char List_Likings[MAX_LIKINGS];
+}User;
+
+
+int menu(){
+    int opt = -1;
+    while (opt == -1){
+        scanf("%d",&opt);
+        if(opt == 1){
+            //Insertar nuevo usuario
+        }
+        else if(opt == 2){
+            //Listar todos los usuarios existentes
+        }
+        else if(opt == 3){
+            //Operar como usuario especifico
+        }
+        else if(opt == 4){
+            //Salir
+        }
+        else {
+            printf("La opción que has elegído no és valida, vuelva a intentar-lo")
+            scanf("%d",&opt);
+        }
+    }
 }
